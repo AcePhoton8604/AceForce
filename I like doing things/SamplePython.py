0bf059aed88728ae0bd5df290bd9e6535e6853b3
AcePhoton <92007531+AcePhoton8604@users.noreply.github.com>
2021-11-09 09:06:08 +0545

    Add files via upload

---
+++ I like doing things/SamplePython.py
@@ -0,0 +1,47 @@
+#vital online compiler: https://www.onlinegdb.com/online_python_compiler
+#next time: https://www.w3schools.com/python/python_datatypes.asp
+#messing about
+x = "So I have started learning python. Huzzah for me!"
+print(x)
+# This is a python comment
+print ("potato")
+" " "One could leave comments like this too, since python only reads it but without a variable doesn't execute(thing) it" " "
+#assigning data to variables
+xa = str(3)
+xb = int(3)
+xc = float(3)
+# printing the variables
+print(xa)
+print(xb)
+print(xc)
+# printing the type of variables
+print (type(xa))
+print (type(xb))
+print (type(xc))
+# experimenting multiple value assigning and printing in one (or two) lines
+aa, ab, ac = 1, 2, 3
+print (aa,ab,ac)
+#Unpacking? That's what this is? Cool!
+flavors = ["blueberry", "strawberry", "cheese", "chocolate", "litchi"]
+o, p, q, r, s = flavors
+print(o, p , q, r, s + "are my favorite flavors in order from first to fifth")
+# working more with the output statments
+s1 = "Python is"
+s2 = " epic "
+s3 = "and no this is not part of "
+s4 =" me practising tutorials, python is really legitimately"
+s5 = s1+s2+s3+s4+s2
+print(s5)
+#functions? yay!
+def functionq():
+      w = "coll"
+      global vry
+      vry = "very, very"
+      print("Functions are "+ w)
+functionq()
+
+    
+
+
+
+

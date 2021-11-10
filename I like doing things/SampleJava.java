094c1b45ad1f038a707ac7f933327d3b54ea7e50
AcePhoton <92007531+AcePhoton8604@users.noreply.github.com>
2021-11-10 14:18:35 +0545

    Update SampleJava.java

--- I like doing things/SampleJava.java
+++ I like doing things/SampleJava.java
@@ -4,9 +4,9 @@ public class Main {
     System.out.println("Hello All! I hope to learn Java properly one day!");
   }
 }
-//I'll write Java here one day when I start learning it
+
 public class Ha {
   public static void Ha(String[] args) {
-    System.out.println("Hello All! I hope to learn Java properly one day!");
+    System.out.println("Javascript syntax scares andd confuses me");
   }
 }

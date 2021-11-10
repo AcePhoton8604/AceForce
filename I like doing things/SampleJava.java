674f7be2a1a181df56e693e9d8914922477f4a2f
AcePhoton <92007531+AcePhoton8604@users.noreply.github.com>
2021-11-10 13:25:28 +0545

    Add files via upload

---
+++ I like doing things/SampleJava.java
@@ -0,0 +1,6 @@
+//I'll write Java here one day when I start learning it
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello All! I hope to learn Java properly one day!");
+  }
+}

bf881fe11bccc8b7857697a25551bd6faa73eb00
AcePhoton <92007531+AcePhoton8604@users.noreply.github.com>
2021-11-10 14:17:30 +0545

    Update SampleJava.java

--- I like doing things/SampleJava.java
+++ I like doing things/SampleJava.java
@@ -4,3 +4,9 @@ public class Main {
     System.out.println("Hello All! I hope to learn Java properly one day!");
   }
 }
+//I'll write Java here one day when I start learning it
+public class Ha {
+  public static void Ha(String[] args) {
+    System.out.println("Hello All! I hope to learn Java properly one day!");
+  }
+}

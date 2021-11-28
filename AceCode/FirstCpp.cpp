3b7b2825fcb7f28f001d79bd0003bd0a46edb152
AcePhoton <92007531+AcePhoton8604@users.noreply.github.com>
2021-11-28 21:39:26 +0545

    Update FirstCpp.cpp

--- AceCode/FirstCpp.cpp
+++ AceCode/FirstCpp.cpp
@@ -1,3 +1,25 @@
-public mainclass void{
-    Banana
-}
+/* https://www.w3schools.com/CPP/cpp_getstarted.asp */
+
+/* C++ is a cross-platform language that can be used to create high-performance applications.
+C++ was developed by Bjarne Stroustrup, as an extension to the C language.
+C++ gives programmers a high level of control over system resources and memory.
+The language was updated 3 major times in 2011, 2014, and 2017 to C++11, C++14, and C++17. */
+
+/* ++ is one of the world's most popular programming languages.
+C++ can be found in today's operating systems, Graphical User Interfaces, and embedded systems.
+C++ is an object-oriented programming language which gives a clear structure to programs and allows code to be reused, lowering development costs.
+C++ is portable and can be used to develop applications that can be adapted to multiple platforms.
+C++ is fun and easy to learn!
+As C++ is close to C# and Java, it makes it easy for programmers to switch to C++ or vice versa */
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Hello World!";
+  return 0;
+} 
+#include <iostream>
+using namespace std;
+int dimain() {
+  cout << "My first C++!";
+  return 0;
+} 

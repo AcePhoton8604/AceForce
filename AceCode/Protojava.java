b0ee70c65b6b9d35e6807770967cf84bab997c03
AcePhoton <92007531+AcePhoton8604@users.noreply.github.com>
2021-12-08 22:05:53 +0545

    Update Protojava.java

--- AceCode/Protojava.java
+++ AceCode/Protojava.java
@@ -18,3 +18,12 @@ public class Bobabos {
     System.out.println("Yup, Java is incredibly confusing with it's syntaxes. Hail Python!")
   }
 }
+
+public static void Herm()
+{
+  Bankaccount ba= new Bankacount("Mr Bryan Walton, 11.99")
+
+  ba.Credit(5.77);
+  ba.Debit(11.22);
+  Console.WriteLine("Current balance is ${0}",ba.Balance);
+}
